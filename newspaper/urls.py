a3c6e16c5d7d24a074f1cab8503241cd6d7457c7
Kamral <kamral010101@gmail.com>
2020-05-25 12:33:24 +0200

    templates

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')), # new
     path('users/', include('django.contrib.auth.urls')), # new
+    path('articles/', include('articles.urls')),
     path('', include('pages.urls')),
+
 ]
